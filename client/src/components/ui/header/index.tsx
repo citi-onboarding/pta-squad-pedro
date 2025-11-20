4413f90b8b88ec50e04528af86be35744e10d310
Lucas Mendonça <lucascmendoncaa@gmail.com>
2025-11-20 13:51:25 -0300

    feat: Add hover and border effects on header buttons

--- client/src/components/ui/header/index.tsx
+++ client/src/components/ui/header/index.tsx
@@ -1,10 +1,14 @@
+"use client";
 import { LogoCITiPet } from "@/assets";
 import { CiTiMessage } from "@/assets";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Header() {
+  const [selectedService, setSelectedService] = useState("");
+
   return (
-    <header className="w-full h-16 relative flex items-center justify-center bg-white shadow-sm px-8">
+    <header className="w-full h-16 relative flex items-center justify-center shadow-sm px-8 py-2">
       <div className="absolute left-8">
         <Image
           src={LogoCITiPet}
@@ -15,8 +19,26 @@ export default function Header() {
         ></Image>
       </div>
       <div className="flex gap-6 items-center">
-        <button>Atendimento</button>
-        <button>Cadastro</button>
+        <button
+          onClick={() => setSelectedService("Atendimento")}
+          className={`hover:text-[#50E678] font-medium transition-colors duration-300 text-base pb-1 border-b-2 ${
+            selectedService === "Atendimento"
+              ? "border-[#50E678] hover:text-black"
+              : "border-transparent"
+          }`}
+        >
+          Atendimento
+        </button>
+        <button
+          onClick={() => setSelectedService("Cadastro")}
+          className={`hover:text-[#50E678] font-medium transition-colors duration-300 text-base pb-1 border-b-2 ${
+            selectedService === "Cadastro"
+              ? "border-[#50E678] hover:text-black"
+              : "border-transparent"
+          }`}
+        >
+          Cadastro
+        </button>
       </div>
       <div className="absolute right-8">
         <Image
@@ -24,7 +46,7 @@ export default function Header() {
           alt="CITiMessage"
           width={220}
           height={24}
-          className="h-6 w-auto object-contain"
+          className="h-5 w-auto object-contain"
         ></Image>
       </div>
     </header>
