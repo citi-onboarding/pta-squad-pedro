ef1ba4dd9fd0354cfa7ed1041a59c437642f41d7
letsx11 <lsx@cin.ufpe.br>
2025-11-20 12:42:15 -0300

    Implement PatientController with CRUD operations
    Primeira implementação do PatientController, com base no UserController

---
+++ server/src/controllers/PatientController.ts
@@ -0,0 +1,51 @@
+import { Request, Response } from "express";
+import { Citi, Crud } from "../global";
+
+class PatientController implements Crud {
+    constructor(private readonly citi = new Citi("Patient")) {}
+    create = async (request: Request, response: Response) => {  
+        const { patientName, patientAge, tutorName, problemDescription } = request.body;
+
+        const isAnyUndefined = this.citi.areValuesUndefined(   
+            patientName,
+            patientAge,
+            tutorName,
+            problemDescription
+        );
+        if (isAnyUndefined) return response.status(400).send();
+
+        const newPatient = { patientName, patientAge, tutorName, problemDescription };
+        const { httpStatus, message } = await this.citi.insertIntoDatabase(newPatient);
+
+        return response.status(httpStatus).send({ message });
+    };
+
+    get = async (request: Request, response: Response) => {
+        const { httpStatus, values } = await this.citi.getAll();
+
+        return response.status(httpStatus).send(values);
+    };
+
+    delete = async (request: Request, response: Response) => {
+        const { id } = request.params;
+
+        const { httpStatus, messageFromDelete } = await this.citi.deleteValue(id);
+
+        return response.status(httpStatus).send({ messageFromDelete });
+    };
+
+    update = async (request: Request, response: Response) => {
+        const { id } = request.params;
+        const { patientName, patientAge, tutorName, problemDescription} = request.body;
+
+        const updatedValues = { patientName, patientAge, tutorName, problemDescription};
+        
+        const { httpStatus, messageFromUpdate } = await this.citi.updateValue(
+            id,
+            updatedValues
+        );
+
+        return response.status(httpStatus).send({ messageFromUpdate });
+    };
+}  
+export default new PatientController();
