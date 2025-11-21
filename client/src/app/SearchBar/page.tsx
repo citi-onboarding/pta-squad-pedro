54ded2339af9507d45e6fd8a62c152faaa50e635
Beatriz Pedrosa <biapedrosa17@gmail.com>
2025-11-21 09:01:27 -0300

    Ajeitando forma de exportar a Search Bar

--- client/src/app/SearchBar/page.tsx
+++ client/src/app/SearchBar/page.tsx
@@ -1,5 +1,5 @@
 import { Input } from "@/components/ui/SearchBar"
 
-export function InputDemo() {
+export default function InputDemo() {
   return <Input type="search" placeholder="Pesquise aqui..." />
 }
