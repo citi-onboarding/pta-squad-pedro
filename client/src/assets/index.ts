1b7ff4fd9069537ad75e0ab2b9f834edb1d0f76b
Beatriz Pedrosa <biapedrosa17@gmail.com>
2025-11-21 11:14:52 -0300

    Ajeitando importar as imagens

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,3 +1,9 @@
 import LogoCITi from "./Logo.png";
+import Cachorro from "./cachorro.svg";
+import Gato from "./gato.svg";
+import Cavalo from "./cavalo.svg";
+import Vaca from "./vaca.svg";
+import Ovelha from "./ovelha.svg";
+import Porco from "./porco.svg";
 
-export { LogoCITi };
+export { LogoCITi, Cachorro, Gato, Cavalo, Vaca, Ovelha, Porco };
