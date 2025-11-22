90a3bbf560c0225e4f2f02f2e5676598ca6699ef
Lucas Mendonça <lucascmendoncaa@gmail.com>
2025-11-22 16:46:51 -0300

    style: Make the default button green

--- client/src/app/testPage/page.tsx
+++ client/src/app/testPage/page.tsx
@@ -1,7 +1,11 @@
 import Header from "@/components/ui/header";
+import { Button } from "@/components/ui/button";
 
 export default function TestPage() {
     return(
-        <Header></Header>
+        <div>
+            <Header></Header>
+            <Button className="m-8">Novo Cadastro</Button>
+        </div>
     )
 }
