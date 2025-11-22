f275cfd6c9faed567eb47a933243072eaee0d01b
Lucas Mendonça <lucascmendoncaa@gmail.com>
2025-11-22 16:58:21 -0300

    style: Create the purple variant

--- client/src/app/testPage/page.tsx
+++ client/src/app/testPage/page.tsx
@@ -3,9 +3,9 @@ import { Button } from "@/components/ui/button";
 
 export default function TestPage() {
     return(
-        <div>
-            <Header></Header>
-            <Button className="m-8">Novo Cadastro</Button>
+        <div className=" h-screen flex flex-row justify-center items-center gap-12">
+            <Button className="">Novo Cadastro</Button>
+            <Button variant={"secondary"}>Buscar</Button>
         </div>
     )
 }
--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
+          "bg-[#8B5CF6] text-zinc-100 shadow-lg shadow-purple-500/20 hover:bg-[#7C3AED] hover:shadow-purple-500/40 active:scale-95 transition-all duration-200",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
