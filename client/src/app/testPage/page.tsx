47761fbbed330930db5671f067d4f83a0efe37fa
Lucas Mendonça <lucascmendoncaa@gmail.com>
2025-11-23 12:25:45 -0300

    feat: add Add and Check icons

--- client/src/app/testPage/page.tsx
+++ client/src/app/testPage/page.tsx
@@ -1,10 +1,13 @@
 import Header from "@/components/ui/header";
 import { Button } from "@/components/ui/button";
+import { Add } from "@/assets/icons";
+import { Check } from "@/assets/icons"
+import Image from "next/image"
 
 export default function TestPage() {
     return(
         <div className=" h-screen flex flex-row justify-center items-center gap-12">
-            <Button className="">Novo Cadastro</Button>
+            <Button> <Image src={Add} alt="Adicionar" width={20} height={20} />Nova Consulta</Button>
             <Button variant={"secondary"}>Buscar</Button>
         </div>
     )
