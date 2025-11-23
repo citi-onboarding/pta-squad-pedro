74245578fafbc30fe18af7eb88d81c47d24d87cb
Lucas Mendonça <lucascmendoncaa@gmail.com>
2025-11-23 13:20:46 -0300

    feat: Import the buttons to test page

--- client/src/app/testPage/page.tsx
+++ client/src/app/testPage/page.tsx
@@ -1,14 +1,23 @@
 import Header from "@/components/ui/header";
 import { Button } from "@/components/ui/button";
-import { Add } from "@/assets/icons";
-import { Check } from "@/assets/icons"
-import Image from "next/image"
+import { CirclePlus } from "lucide-react";
+import { CircleCheckBig } from "lucide-react";
+
 
 export default function TestPage() {
-    return(
-        <div className=" h-screen flex flex-row justify-center items-center gap-12">
-            <Button> <Image src={Add} alt="Adicionar" width={20} height={20} />Nova Consulta</Button>
-            <Button variant={"secondary"}>Buscar</Button>
-        </div>
-    )
+  return (
+    <div className=" h-screen flex flex-row justify-center items-center gap-12">
+      <Button className="flex items-center gap-2">
+        <CirclePlus className="text-zinc-100"></CirclePlus>
+        Nova Consulta
+      </Button>
+      <Button className="flex items-center gap-2">
+        <CircleCheckBig></CircleCheckBig>
+        Agendamento
+      </Button>
+      <Button>Enviar</Button>
+      <Button>Finalizar Cadastro</Button>
+      <Button variant={"secondary"}>Buscar</Button>
+    </div>
+  );
 }
