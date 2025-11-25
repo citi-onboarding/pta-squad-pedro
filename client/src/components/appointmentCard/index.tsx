a255cbe85eda89ac691f98cc7b70cb7182c1cda0
Lucas Mendonça <lucascmendoncaa@gmail.com>
2025-11-25 18:02:07 -0300

    feat: Add the clock hour and date div

--- client/src/components/appointmentCard/index.tsx
+++ client/src/components/appointmentCard/index.tsx
@@ -10,9 +10,13 @@ const mockAppointment = {
 
 export default function AppointmentCard() {
   return (
-    <div className="h-[135px] w-[495px] bg-[#BFB5FF] rounded-[16px]">
-      <div className="">
+    <div className="h-[135px] w-[495px] bg-[#BFB5FF] rounded-[16px] flex justify-between items-center px-6">
+      <div className=" bg-zinc-100 w-[51px] h-[90px] rounded-[4px] flex flex-col items-center p-2.5">
             <AlarmClock></AlarmClock>
+            <div className="font-semibold text-sm">
+                <p>{mockAppointment.date}</p>
+                <p>{mockAppointment.hour}</p>
+            </div>
       </div>
     </div>
   );
