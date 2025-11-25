e41ec49cbdb16056f3a7c8d2caf21b7778fec04b
Lucas Mendonça <lucascmendoncaa@gmail.com>
2025-11-25 19:28:28 -0300

    fix: Code formatting adjustments

--- client/src/app/testPage/page.tsx
+++ client/src/app/testPage/page.tsx
@@ -5,12 +5,11 @@ export default function TestPage() {
 
     <div className="min-h-screen w-full flex justify-center items-start bg-gray-50 p-8">
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-2 gap-6 w-full max-w-7xl">
-        
         <AppointmentCard appointment="Primeira Consulta" status="available" />
         <AppointmentCard appointment="Retorno" status="available" />
         <AppointmentCard appointment="Check-up" status="available" />
         <AppointmentCard appointment="Vacinação" status="available" />
-        
+        <AppointmentCard appointment="Vacinação" status="late" />
       </div>
     </div>
   );
