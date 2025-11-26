389d56de4ab0afa0ebaf71085f6936c8344db1d9
Pedro Campelo <pedro.fev03@gmail.com>
2025-11-26 20:00:01 -0300

    main: creating axios api routing.

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(routes);
+app.use("/api", routes);
 app.use(express.static(__dirname + "/public"));
 
 app.listen(process.env.SERVER_PORT || 3001, () => {
