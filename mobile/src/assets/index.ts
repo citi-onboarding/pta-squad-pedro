a29a74b4bea26fcbc7355726fb2e8a642a6f4855
Lucas Mendonça <lucascmendoncaa@gmail.com>
2025-12-03 18:08:54 -0300

    feat: Add the mobile shift icons

--- mobile/src/assets/index.ts
+++ mobile/src/assets/index.ts
@@ -1,5 +1,10 @@
 import citi from './citi.svg';
+import MorningIcon from "./Sun Fog.svg"
+import AfternoonIcon from "./Vector_3.svg"
+import EveningIcon from "./Moon-Stars--Streamline-Solar.svg"
 
 export {
   citi,
 };
+
+export {MorningIcon, AfternoonIcon, EveningIcon};
--- mobile/src/components/ShiftSwitcher/index.tsx
+++ mobile/src/components/ShiftSwitcher/index.tsx
@@ -1,3 +1,20 @@
+import { TouchableOpacity, View, Image } from "react-native";
+import { MorningIcon } from "@assets";
+
 export default function ShiftSwitcher() {
-    return <h1>oi</h1>
+    return (
+        <View className="flex flex-row gap-4">
+            <TouchableOpacity>
+                <MorningIcon width={24} height={24} />
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+                <MorningIcon width={24} height={24} />
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+                <MorningIcon width={24} height={24} />
+            </TouchableOpacity>
+        </View>
+    )
 }
